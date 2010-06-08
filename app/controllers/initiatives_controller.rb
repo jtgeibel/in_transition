15498f0222bf5443868b0049f0c5d6eb563d84b3
Justin <jtgeibel@gmail.com>
2010-06-08 18:30:24 -0400

    Saving work on refactoring the controller.  May be worth imporoving this in the generator.

--- app/controllers/initiatives_controller.rb
+++ app/controllers/initiatives_controller.rb
@@ -1,76 +1,37 @@
 class InitiativesController < ApplicationController
-  # GET /initiatives
-  # GET /initiatives.xml
+  respond_to :html, :xml, :json
+
   def index
     @initiatives = Initiative.all
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.xml  { render :xml => @initiatives }
-    end
+    respond_with @initiatives
   end
 
-  # GET /initiatives/1
-  # GET /initiatives/1.xml
   def show
     @initiative = Initiative.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.xml  { render :xml => @initiative }
-    end
+    respond_with @initiative
   end
 
-  # GET /initiatives/new
-  # GET /initiatives/new.xml
   def new
     @initiative = Initiative.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.xml  { render :xml => @initiative }
-    end
+    respond_with @initiative
   end
 
-  # GET /initiatives/1/edit
   def edit
     @initiative = Initiative.find(params[:id])
   end
 
-  # POST /initiatives
-  # POST /initiatives.xml
   def create
     @initiative = Initiative.new(params[:initiative])
-
-    respond_to do |format|
-      if @initiative.save
-        format.html { redirect_to(@initiative, :notice => 'Initiative was successfully created.') }
-        format.xml  { render :xml => @initiative, :status => :created, :location => @initiative }
-      else
-        format.html { render :action => "new" }
-        format.xml  { render :xml => @initiative.errors, :status => :unprocessable_entity }
-      end
-    end
+    flash[:notice] = 'Initiative was successfully created.' if @initiative.save
+    respond_with(@initiative)
   end
 
-  # PUT /initiatives/1
-  # PUT /initiatives/1.xml
   def update
     @initiative = Initiative.find(params[:id])
-
-    respond_to do |format|
-      if @initiative.update_attributes(params[:initiative])
-        format.html { redirect_to(@initiative, :notice => 'Initiative was successfully updated.') }
-        format.xml  { head :ok }
-      else
-        format.html { render :action => "edit" }
-        format.xml  { render :xml => @initiative.errors, :status => :unprocessable_entity }
-      end
-    end
+    flash[:notice] = 'Initiative was successfully updated.' if @initiative.update_attributes(params[:initiative])
+    respond_with(@initiative)
   end
 
-  # DELETE /initiatives/1
-  # DELETE /initiatives/1.xml
   def destroy
     @initiative = Initiative.find(params[:id])
     @initiative.destroy
