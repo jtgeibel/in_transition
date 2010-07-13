ce8e0ad83f29c29ffd5c37344429020dfcb9a2a4
Justin <jtgeibel@gmail.com>
2010-07-12 22:41:12 -0400

    Override create and update actions with restful code.  InheritedResource doesn't like my class to be named Resource.  I think this stems from a conflict of InheritedResources helpers and the routers resources_url helper.

--- app/controllers/resources_controller.rb
+++ app/controllers/resources_controller.rb
@@ -1,27 +1,12 @@
 class ResourcesController < InheritedResources::Base
-#   respond_to :html
-#   
-#   def create
-#     @resource = Resource.create(params[:resource])
-#     respond_with(@resource, :location => resources_url)
-#   end
-#   
-#   def new
-#     respond_with(@resource = Resource.new)
-#   end
-#   
-#   def index
-#     respond_with(@resources = Resource.all)
-#   end
-#   
-#   def show
-#     respond_with(@resource = Resource.find(params[:id]))
-#   end
-#   
-#   def edit
-#     respond_with(@resource = Resource.find(params[:id]))
-#   end
-#   
-#   def destroy
-#   end
+  def create
+    @resource = Resource.create(params[:resource])
+    respond_with(@resource, :location => resources_url)
+  end
+
+  def update
+    @resource = Resource.find(params[:id])
+    @resource.update_attributes(params[:resource])
+    respond_with(@resource, :location => resources_url)
+  end
 end
--- app/models/resource.rb
+++ app/models/resource.rb
@@ -1,3 +1,5 @@
 class Resource < ActiveRecord::Base
   belongs_to :focus_area
+
+  validates_length_of :title, :minimum=>3
 end
